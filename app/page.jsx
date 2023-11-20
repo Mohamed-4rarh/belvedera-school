9903d53002b85bdd219e268f15e939d9b765598a
mohamed-4rarh <mainmohedadham@gmail.com>
2023-11-20 13:23:02 +0200

    life - home page pc

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,14 @@ import styles from './page.module.css'
 import Link from 'next/link'
 import Hero from '@/containers/home/Hero'
 import History from '@/containers/home/History'
+import Life from '@/containers/home/Life'
 
 export default function Home() {
   return(
     <>
       <Hero />
       <History />
+      <Life />
     </>
   )
 }
---
+++ containers/home/Life.jsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import React from 'react'
+import '@/styles/life.css'
+
+export default function Life() {
+  return (
+    <section className='life__section'>
+        <div className='container'>
+            <div className='life__content'>
+                <span>Life & learning</span>
+                <h2>Academic and Extra <br /> Curriculum</h2>
+                <p>
+                    At Belvedere School, Cairo pupils take ownership of what they learn by focusing on how the new knowledge solves a problem or adds value, instead of simply being given information and waiting for the correct answer. This preps them for their ever-changing future. Many of the jobs which our young people will be applying for in years to come have not even been invented yet.
+                    <br />
+                    We believe the ability to develop critical thinking skills and thinking flexibility, is vital.
+                </p>
+            </div>
+            <div className='life__image'>
+                <Image 
+                    src={'/lifeHome.jpg'}
+                    fill
+                    unoptimized
+                />
+            </div>
+        </div>
+    </section>
+  )
+}
