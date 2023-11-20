235e8470e0bee03735e7db1c4660ccfc89f759c7
mohamed-4rarh <mainmohedadham@gmail.com>
2023-11-20 13:01:07 +0200

    history - home

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Link from 'next/link'
-import Hero from '@/containers/Hero'
+import Hero from '@/containers/home/Hero'
+import History from '@/containers/home/History'
 
 export default function Home() {
   return(
     <>
       <Hero />
+      <History />
     </>
   )
 }
