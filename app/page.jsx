50e5d631d5b12eee5036835234bb7ab42036341c
mohamed-4rarh <mainmohedadham@gmail.com>
2023-11-23 23:25:40 +0200

    edit folder structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,3 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Link from 'next/link'
 import Hero from '@/containers/home/Hero'
 import History from '@/containers/home/History'
 import Life from '@/containers/home/Life'
