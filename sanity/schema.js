3eb0c8ea3a23d44bda2825dee5ad594126ecdbdb
mohamed-4rarh <mainmohedadham@gmail.com>
2023-11-24 21:34:00 +0200

    about-schema

--- sanity/schema.js
+++ sanity/schema.js
@@ -2,7 +2,8 @@ import {blockContent} from './schemas/blockContent'
 import {category} from './schemas/category'
 import {post} from './schemas/post'
 import {author} from './schemas/author'
+import { aboutPage } from './schemas/about_us_pages/about'
 
 export const schema = {
-  types: [post, author, category, blockContent],
+  types: [post, author, category, blockContent, aboutPage],
 }
--- sanity/schemas/about_us_pages/about.js
+++ sanity/schemas/about_us_pages/about.js
@@ -1,1 +1,47 @@
-import { defineType, defineField } from "sanity";
+export const aboutPage = {
+    name: 'aboutUsPage',
+    title: 'About Us',
+    type: 'document',
+    fields: [
+        {
+            name: 'mainImage',
+            title: 'Hero Image',
+            type: 'image'
+        },
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'header',
+            title: 'Header',
+            type: 'string'
+        },
+        {
+            name: 'text',
+            title: 'Text',
+            type: 'text'
+        },
+        {
+            name: 'vision',
+            title: 'Our Vision',
+            type: 'text',
+        },
+        {
+            name: 'mission',
+            title: 'Our Mission',
+            type: 'text'
+        },
+        {
+            name: 'beliefs',
+            title: 'Beliefs',
+            type: 'text'
+        },
+        {
+            name: 'bodyImage',
+            title: 'Body Image',
+            type: 'image',
+        }
+    ]
+}
